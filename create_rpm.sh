5bc962207eecd60930ff03be42adff15785c57cb
Ramprasad Prabhakar <ramprasad@apple.com>
2020-08-20 19:31:03 -0700

    fixed

--- create_rpm.sh
+++ create_rpm.sh
@@ -16,7 +16,9 @@ name=`grep -P '^Name:\s+' $specfile |awk '{print $2}'`
 commitdate=`git log -1 --format="%ct"`
 version=`grep -P '^Version:\s+' $specfile |awk '{print $2}'`.$commitdate
 #release=`grep -P '^Release:\s+' rpm/$specfile |awk '{print $2}'`
-
+gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -
 rm -rf rpmbuild
 mkdir -p rpmbuild/{BUILD,RPMS,SOURCES/$name-$version,SPECS,SRPMS,tmp}
 cp -a $specfile rpmbuild/SPECS
