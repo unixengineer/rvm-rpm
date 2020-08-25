4d1b6f1724083f966da12323d9f8b9ba43ec8b87
Ramprasad Prabhakar <ramprasad@apple.com>
2020-08-25 16:29:51 -0700

    updated path

--- local_installer.sh
+++ local_installer.sh
@@ -2,11 +2,12 @@
 RUBY_VERSION="${RUBY_VERSION:-2.7.1}"
 RUBYGEMS_VERSION="${RUBYGEMS_VERSION:-3.1.4}"
 YAML_VERSION="${YAML_VERSION:-0.1.6}"
+RVM_VERSION="${RVM_VERSION:-1.29.10}"
 # When moving to Artifactory, change the URL accordingly
-RVM_DOWNLOAD_URL="https://github.com/rvm/rvm/archive/${RVM_VERSION:-1.29.10}.tar.gz"
-RUBYGEMS_DOWNLOAD_URL="http://production.cf.rubygems.org/rubygems"
+RVM_DOWNLOAD_URL="https://github.com/rvm/rvm/archive/${RVM_VERSION}.tar.gz"
+RUBYGEMS_DOWNLOAD_URL="http://production.cf.rubygems.org/rubygems/rubygems-${RUBYGEMS_VERSION}.tgz"
 RUBY_DOWNLOAD_URL="https://rvm_io.global.ssl.fastly.net/binaries/centos/7/x86_64/ruby-${RUBY_VERSION}.tar.bz2"
-YAML_DOWNLOAD_URL="http://pyyaml.org/download/libyaml"
+YAML_DOWNLOAD_URL="http://pyyaml.org/download/libyaml/yaml-${YAML_VERSION}.tar.gz"
 
 ###
 RVM_ARCHIVE_PATH="/usr/local/rvm/archives"
@@ -16,7 +17,7 @@ yum -y install wget sudo java-11-openjdk-devel git rpm-build redhat-rpm-config c
 #yum clean all && yum -y groupinstall "Development tools"
 
 # Now install RVM
-curl -sSL ${RVM_DOWNLOAD_URL} -o rvm-stable.tar.gz
+curl ${RVM_DOWNLOAD_URL} -o rvm-stable.tar.gz
 mkdir rvm && cd rvm
 tar --strip-components=1 -xzf ../rvm-stable.tar.gz
 ./install --auto-dotfiles # RVM will be installed in /usr/local/rvm
@@ -30,10 +31,9 @@ fi
 
 [[ -d ${RVM_ARCHIVE_PATH} ]] || mkdir -p ${RVM_ARCHIVE_PATH}
 curl ${RUBY_DOWNLOAD_URL} -o ${RVM_ARCHIVE_PATH}/ruby-${RUBY_VERSION}.tar.bz2
-curl ${RUBYGEMS_DOWNLOAD_URL}/rubygems-${RUBYGEMS_VERSION}.tgz -o ${RVM_ARCHIVE_PATH}/rubygems-${RUBYGEMS_VERSION}.tgz
-curl ${YAML_DOWNLOAD_URL}/yaml-${YAML_VERSION}.tar.gz -o ${RVM_ARCHIVE_PATH}/yaml-${YAML_VERSION}.tar.gz
+curl ${RUBYGEMS_DOWNLOAD_URL} -o ${RVM_ARCHIVE_PATH}/rubygems-${RUBYGEMS_VERSION}.tgz
+curl ${YAML_DOWNLOAD_URL} -o ${RVM_ARCHIVE_PATH}/yaml-${YAML_VERSION}.tar.gz
 
-ls -la ${RVM_ARCHIVE_PATH}/
 
 
 source /etc/profile.d/rvm.sh
