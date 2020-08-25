77c34c065d323241252a86f62704842680b940c8
Ramprasad Prabhakar <ramprasad@apple.com>
2020-08-25 16:25:39 -0700

    updated path

--- local_installer.sh
+++ local_installer.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-set -x
-
 RUBY_VERSION="${RUBY_VERSION:-2.7.1}"
-RUBY_DOWNLOAD_URL="https://rvm_io.global.ssl.fastly.net/binaries/centos/7/x86_64/ruby-${RUBY_VERSION}.tar.bz2"
 RUBYGEMS_VERSION="${RUBYGEMS_VERSION:-3.1.4}"
-RUBYGEMS_DOWNLOAD_URL="http://production.cf.rubygems.org/rubygems"
 YAML_VERSION="${YAML_VERSION:-0.1.6}"
+# When moving to Artifactory, change the URL accordingly
+RVM_DOWNLOAD_URL="https://github.com/rvm/rvm/archive/${RVM_VERSION:-1.29.10}.tar.gz"
+RUBYGEMS_DOWNLOAD_URL="http://production.cf.rubygems.org/rubygems"
+RUBY_DOWNLOAD_URL="https://rvm_io.global.ssl.fastly.net/binaries/centos/7/x86_64/ruby-${RUBY_VERSION}.tar.bz2"
 YAML_DOWNLOAD_URL="http://pyyaml.org/download/libyaml"
+
+###
 RVM_ARCHIVE_PATH="/usr/local/rvm/archives"
 # Install the yum dependencies to build rvm
 yum -y install wget sudo java-11-openjdk-devel git rpm-build redhat-rpm-config chrpath readline-devel zlib-devel libyaml-devel libffi-devel openssl-devel which sqlite-devel
@@ -14,7 +16,7 @@ yum -y install wget sudo java-11-openjdk-devel git rpm-build redhat-rpm-config c
 #yum clean all && yum -y groupinstall "Development tools"
 
 # Now install RVM
-curl -sSL https://github.com/rvm/rvm/tarball/stable -o rvm-stable.tar.gz
+curl -sSL ${RVM_DOWNLOAD_URL} -o rvm-stable.tar.gz
 mkdir rvm && cd rvm
 tar --strip-components=1 -xzf ../rvm-stable.tar.gz
 ./install --auto-dotfiles # RVM will be installed in /usr/local/rvm
@@ -30,7 +32,7 @@ fi
 curl ${RUBY_DOWNLOAD_URL} -o ${RVM_ARCHIVE_PATH}/ruby-${RUBY_VERSION}.tar.bz2
 curl ${RUBYGEMS_DOWNLOAD_URL}/rubygems-${RUBYGEMS_VERSION}.tgz -o ${RVM_ARCHIVE_PATH}/rubygems-${RUBYGEMS_VERSION}.tgz
 curl ${YAML_DOWNLOAD_URL}/yaml-${YAML_VERSION}.tar.gz -o ${RVM_ARCHIVE_PATH}/yaml-${YAML_VERSION}.tar.gz
-set +x
+
 ls -la ${RVM_ARCHIVE_PATH}/
 
 
